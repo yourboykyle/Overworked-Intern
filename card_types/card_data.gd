4aa651a79cb4998ff08d2d9ef11635ab7b7f710b
Kyle Lyon <kyle.lyon2542@gmail.com>
2026-02-09 13:25:03 -0500

    Created a working ReversibleCard object

---
+++ card_types/card_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name CardData
+
+@export var card_name: String
+@export var front_texture: Texture2D
+@export var back_texture: Texture2D
---
+++ scenes/reversible_card.gd
@@ -0,0 +1,35 @@
+@tool
+extends Node2D
+
+@export var card_data: CardData
+@export var is_face_up := true
+
+@onready var front = $Front
+@onready var back = $Back
+
+func _ready():
+	apply_card_data()
+	update_visual()
+
+func apply_card_data():
+	if card_data:
+		front.texture = card_data.front_texture
+		back.texture = card_data.back_texture
+
+func flip():
+	is_face_up = !is_face_up
+	update_visual()
+
+func update_visual():
+	front.visible = is_face_up
+	back.visible = !is_face_up
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		flip()
+
+func _process(_delta):
+	if Engine.is_editor_hint():
+		apply_card_data()
+		update_visual()
