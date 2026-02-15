3f4ccf695b51cdabeb79d5d58a8a41f640564f42
Kyle Lyon <kyle.lyon2542@gmail.com>
2026-02-15 12:04:16 -0500

    Added memory card flipping functionality, along with a draw pile

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,6 +9,12 @@ extends Node2D
 
 @onready var grid = $GridContainer
 @onready var draw_pile = $DrawPile
+@onready var draw_pile_label = $UIElements/DrawPileCount
+
+var flipped_cards: Array = []
+var checking_match := false
+
+@onready var match_slots = $MatchedPile
 
 func _ready():
 	shuffle_and_deal()
@@ -54,13 +60,17 @@ func deal_grid(cards):
 		var card = card_scene.instantiate()
 		card.card_data = cards[i]
 		card.is_reversible = true
+		card.connect("card_flipped", _on_card_flipped)
+		card.add_to_group("memory_cards")
 
 		card.position = Vector2(
 			col * spacing.x,
 			row * spacing.y
 		)
 
+		card.grid_index = i
 		grid.add_child(card)
+		card.set_face_down()
 
 func deal_pile(cards):
 	var offset = Vector2(0, 0)
@@ -69,11 +79,15 @@ func deal_pile(cards):
 		var card = card_scene.instantiate()
 		card.card_data = cards[i]
 		card.is_reversible = true
+		card.connect("card_flipped", _on_card_flipped)
+		card.add_to_group("memory_cards")
 
 		card.position = offset * i
 		draw_pile.add_child(card)
 		
 		card.set_face_down()
+	
+	update_draw_pile_label()
 
 func _on_shuffle_pressed() -> void:	
 	shuffle_and_deal()
@@ -82,3 +96,87 @@ func _on_shuffle_pressed() -> void:
 
 func _on_board_player_room_changed(room: String) -> void:
 	$UIElements/CurrentRoomLabel.text = room
+
+func _on_card_flipped(card):
+	if checking_match:
+		return
+
+	if not card.is_face_up:
+		return
+
+	flipped_cards.append(card)
+
+	if flipped_cards.size() == 2:
+		checking_match = true
+		lock_all_cards()
+		check_match()
+
+func check_match():
+	var card1 = flipped_cards[0]
+	var card2 = flipped_cards[1]
+
+	if card1.card_data == card2.card_data:
+		await get_tree().create_timer(0.4).timeout
+
+		var index1 = card1.grid_index
+		var index2 = card2.grid_index
+
+		move_to_match_area(card1)
+		move_to_match_area(card2)
+
+		draw_card_from_pile(index1)
+		draw_card_from_pile(index2)
+	else:
+		await get_tree().create_timer(0.8).timeout
+		card1.force_flip()
+		card2.force_flip()
+
+	flipped_cards.clear()
+	checking_match = false
+	unlock_all_cards()
+
+func move_to_match_area(card):
+	card.get_parent().remove_child(card)
+	match_slots.add_child(card)
+
+	var offset = Vector2(0, 0)
+	card.position = offset * match_slots.get_child_count()
+	card.is_reversible = false
+
+func lock_all_cards():
+	for card in get_tree().get_nodes_in_group("memory_cards"):
+		card.can_flip = false
+
+func unlock_all_cards():
+	for card in get_tree().get_nodes_in_group("memory_cards"):
+		card.can_flip = true
+
+func draw_card_from_pile(index):
+	if draw_pile.get_child_count() == 0:
+		return
+	
+	var new_card = draw_pile.get_child(0)
+	draw_pile.remove_child(new_card)
+	grid.add_child(new_card)
+
+	new_card.grid_index = index
+	new_card.position = get_grid_position(index)
+	new_card.set_face_down()
+	new_card.is_reversible = true
+	
+	update_draw_pile_label()
+
+func get_grid_position(index):
+	var cols = 7
+	var spacing = Vector2(160, 160)
+
+	var row = index / cols
+	var col = index % cols
+
+	return Vector2(
+		col * spacing.x,
+		row * spacing.y
+	)
+
+func update_draw_pile_label():
+	draw_pile_label.text = "(" + str(draw_pile.get_child_count()) + ")"
--- scripts/playing_card.gd
+++ scripts/playing_card.gd
@@ -1,6 +1,8 @@
 @tool
 extends Node2D
 
+signal card_flipped(card)
+
 @export var card_data: CardData
 @export var is_face_up := true
 @export var in_deck := true
@@ -9,9 +11,11 @@ extends Node2D
 @onready var front = $Front
 @onready var back = $Back
 
+@export var can_flip := true
+
+var grid_index: int = -1
+
 func _ready():
-	if in_deck:
-		add_to_group("deck_cards")
 	apply_card_data()
 	update_visual()
 
@@ -21,9 +25,16 @@ func apply_card_data():
 		back.texture = card_data.back_texture
 
 func flip():
-	if is_reversible:
-		is_face_up = !is_face_up
-		update_visual()
+	if not is_reversible or not can_flip:
+		return
+
+	is_face_up = !is_face_up
+	update_visual()
+	emit_signal("card_flipped", self)
+
+func force_flip():
+	is_face_up = !is_face_up
+	update_visual()
 
 func set_face_up():
 	is_face_up = true
